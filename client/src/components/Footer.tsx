68ec7c5f26523eefec033b5d1f652dd8bfcd69c1
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-06-06 23:31:57 +0630

    Footer ui implement

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const date = new Date();
 
   return (
-    <footer className="bottom-0 left-0 w-full bg-gray-800 text-center mt-2 mb-1 py-2 text-gray-200 z-50">
+    <footer className="bottom-0 fixed left-0 w-full bg-gray-700 text-center mt-2 py-2 text-gray-200 z-50">
       © {date.getFullYear()} . Made with love by{" "}
       <Link
         to="https://www.google.com/"
